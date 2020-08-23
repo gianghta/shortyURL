855ca55cf88a9e5d859e79e70b9c7b728d11c5e3
Giang Ta <thoanggiang98@gmail.com>
2020-08-22 22:18:39 -0400

    modify front page

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './tailwind.output.css';
 
-const App = () => (
-	<div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
-		<div className="ml-6 pt-1">
-			<h1 className="text-2xl text-blue-700 leading-tight">Tailwind and Create React App</h1>
-			<p className="text-base text-gray-700 leading-normal">Building apps together</p>
+const App = () => {
+	return (
+		<div class="flex items-center justify-center h-screen">
+			<div class="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+				<div class="relative mr-6 my-2">
+					<input type="search" class="bg-purple-white shadow rounded border-0 p-3" placeholder="Search by name..." />
+					<div class="absolute pin-r pin-t mt-3 mr-4 text-purple-lighter">
+						<svg
+							version="1.1"
+							class="h-4 text-dark"
+							xmlns="http://www.w3.org/2000/svg"
+							xmlnsXlink="http://www.w3.org/1999/xlink"
+							x="0px"
+							y="0px"
+							viewBox="0 0 52.966 52.966"
+							style={{ 'enable-background': 'new 0 0 52.966 52.966' }}
+							xmlSpace="preserve"
+						>
+							<path d="M51.704,51.273L36.845,35.82c3.79-3.801,6.138-9.041,6.138-14.82c0-11.58-9.42-21-21-21s-21,9.42-21,21s9.42,21,21,21
+                    c5.083,0,9.748-1.817,13.384-4.832l14.895,15.491c0.196,0.205,0.458,0.307,0.721,0.307c0.25,0,0.499-0.093,0.693-0.279
+                    C52.074,52.304,52.086,51.671,51.704,51.273z M21.983,40c-10.477,0-19-8.523-19-19s8.523-19,19-19s19,8.523,19,19
+                    S32.459,40,21.983,40z" />
+						</svg>
+					</div>
+				</div>
+			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 ReactDOM.render(<App />, document.querySelector('#root'));
